fc89d26b80f456c220e3f06afb4c07bc82bd9070
Peter Stace <peterstace@gmail.com>
2017-07-27 13:02:52 +1000

    Update settings

---
+++ README.md
@@ -0,0 +1,8 @@
+# Buddhabrot
+
+This is a small C program written in ~2009. It renders the
+[Buddhabrot](git@github.com:peterstace/buddhabrot.git) fractal.
+
+Example output:
+
+TODO
--- src/SETTINGS.h
+++ src/SETTINGS.h
@@ -1,11 +1,11 @@
 
 /* DIMENSIONS */
-#define WIDTH_PX 1200
-#define HEIGHT_PX 1920
-#define MIN_X -2.1
-#define MAX_X 1.9
-#define MIN_Y -3.2
-#define MAX_Y 3.2
+#define WIDTH_PX 500
+#define HEIGHT_PX 500
+#define MIN_X -1.5
+#define MAX_X 0.75
+#define MIN_Y -1.125
+#define MAX_Y 1.125
 
 /* PERSISTANT DATA LOCATION */
 #define PDATA_LOC "data.dat"
@@ -14,7 +14,7 @@
 /* RENDER OPTIONS */
 #define BAILOUT 5.0
 #define MAX_ITER 20000
-#define FORMULA is_pow4_pc
+#define FORMULA is_pow2_pc
 #define NUM_THREADS 8
-#define IN_SET false
+#define IN_SET true
 
